9b548a2683a9ee46b1191f64f18a605fa3eeb215
c0dem0de <92681641+c0dem0de@users.noreply.github.com>
2024-04-07 00:02:22 +0530

    Update omni_m30s.mk

--- omni_m30s.mk
+++ omni_m30s.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit from m30s device
 $(call inherit-product, device/samsung/m30s/device.mk)
