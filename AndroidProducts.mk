63fe3e9bee538649e80e1a4c1d11fa0c89345697
c0dem0de <codemode127@gmail.com>
2024-04-06 18:36:26 +0530

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m30s.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m30s-user \
+    omni_m30s-userdebug \
+    omni_m30s-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for samsung SM-M307F (m30s)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
