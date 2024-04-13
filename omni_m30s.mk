59d4fd9a9bcc67a348c452f8df94ed9bad44402d
DeadPool-4422 <123509971+DeadPool-4422@users.noreply.github.com>
2024-04-13 17:19:55 +0530

    Update omni_m30s.mk

--- omni_m30s.mk
+++ omni_m30s.mk
@@ -17,14 +17,17 @@
 # Release name
 PRODUCT_RELEASE_NAME := m30s
 
-$(call inherit-product, build/target/product/embedded.mk)
-$(call inherit-product, build/target/product/aosp_base.mk)
-
-# Inherit from our custom product configuration
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Include any options that can't be included in BoardConfig.mk
 $(call inherit-product, device/samsung/m30s/device.mk)
+
+# Inherit some common PitchBlack stuff.
+$(call inherit-product, vendor/pb/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := m30s
